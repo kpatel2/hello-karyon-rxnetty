437a47d9676117b2bc15d06c46f1861186eabd59
Kalpesh Patel <kalpesh_patel2@intuit.com>
2017-05-08 18:24:57 -0400

    changing hello page

--- src/main/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpoint.java
+++ src/main/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpoint.java
@@ -27,7 +27,7 @@ import rx.Observable;
 public class HelloEndpoint {
 
     public Observable<String> getHello() {
-        return Observable.just("Hello Spinnaker V3.0");
+        return Observable.just("Hello Spinnaker V4.0");
     }
 
     public Observable<String> getHelloName(HttpServerRequest<ByteBuf> request) {
